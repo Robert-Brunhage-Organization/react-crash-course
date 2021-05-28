04428bbc3483b2776890635d91812fa68e6d855b
Robert Brunhage <robertbrunhage@gmail.com>
2021-05-28 17:02:21 +0200

    Add all the components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useRef, useState } from "react";
-import { TrashIcon } from "@heroicons/react/outline";
 import Headline from "./components/Headline";
 import Input from "./components/Input";
 import Button from "./components/Button";
@@ -36,33 +35,12 @@ function App() {
 
   return (
     <div className="w-screen h-screen flex flex-col items-center space-y-8 pt-40">
-      <h1 className="text-4xl font-medium">Todo List</h1>
+      <Headline>Todo list</Headline>
       <div className="flex space-x-2">
-        <input
-          data-testid="todo-input"
-          className="border rounded py-2 px-4"
-          ref={nameRef}
-          type="text"
-        />
-        <button
-          className="bg-indigo-500 py-2 px-4 rounded text-white hover:bg-indigo-700"
-          onClick={() => addTodo()}
-        >
-          Add todo
-        </button>
+        <Input ref={nameRef}></Input>
+        <Button onClick={() => addTodo()}>Add todo</Button>
       </div>
-      {todos.map((value, index) => {
-        return (
-          <div className="flex justify-between w-80">
-            <p>{value}</p>
-            <TrashIcon
-              data-testid={`delete-${value}`}
-              className="w-6 h-6 text-gray-400 cursor-pointer hover:text-gray-700"
-              onClick={() => removeTodo(index)}
-            ></TrashIcon>
-          </div>
-        );
-      })}
+      <Todos todos={todos} removeTodo={removeTodo}></Todos>
     </div>
   );
 }
